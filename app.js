3c0a8f99350e28234073c46335d422584abaf1a5
Arun Singh <57107878+arunsingh-me@users.noreply.github.com>
2023-03-12 20:26:28 +0530

    Modified MongoDB connection for serverless

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 //import routes
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+  require("dotenv").config();
 }
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Database Connection
-mongoose
-    .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-    .then(() => console.log("Connected to the Database"))
-    .catch((error) => console.error(error));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// mongoose
+//     .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+//     .then(() => console.log("Connected to the Database"))
+//     .catch((error) => console.error(error));
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//     console.log(`App is listening on PORT ${PORT}`);
+// });
+//OLD method
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`);
+  });
 });
